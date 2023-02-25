a577a853bcecd4eed54725715d2339d1b757c81a
mierudayo <webalpa14@gmail.com>
2023-02-25 11:25:00 +0900

    sequelize.import() の使用を止める

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
+const {Sequelize, DataTypes} = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
@@ -22,7 +22,10 @@ fs
   })
   .forEach(file => {
     console.log("loading " + file);
-    const model = sequelize['import'](path.join(__dirname, file));
+    // sequelize.import() is deprecated
+    // https://sequelize.org/docs/v6/moved/models-definition/#deprecated-sequelizeimport
+    const modelFunc = require(path.join(__dirname, file));
+    const model = modelFunc(sequelize, DataTypes);
     console.log(model);
     db[model.name] = model;
   });
